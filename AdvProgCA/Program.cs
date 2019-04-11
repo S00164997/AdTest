8b374cccd1e1a752798436bf5418e096e0e64e53
Michael Chrystal <S00164997@mail.itsligo.ie>
2019-04-11 14:09:16 +0100

    Add project files.

---
+++ AdvProgCA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvProgCA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
